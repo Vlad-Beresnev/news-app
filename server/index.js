c4b47fdc4695316b8d3ca9d2818a885c7cd49f3d
Vladislav Beresnev <128588731+Vlad-Beresnev@users.noreply.github.com>
2024-02-24 12:27:23 +0200

    build echo no build step

--- server/index.js
+++ server/index.js
@@ -21,6 +21,8 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from servers!" });
 });
@@ -47,6 +49,10 @@ app.get("/news", async (req, res) => {
         }
     }
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
